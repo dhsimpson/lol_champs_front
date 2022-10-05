b5b525a8e3693cd54e00d9d026accce9ada402c2
yundonghui <dhsimpson@donnie4567.com>
2022-10-06 03:02:56 +0900

    feat | 정렬 영역 퍼블리싱

--- src/components/videoListComponents/CountAndSortComponent.vue
+++ src/components/videoListComponents/CountAndSortComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="count-and-sort-wrapper">
     <div class="count-and-sort-container">
-      <span>10 개의 검색 결과</span>
+      <span>10 개의 경기 동영상</span>
       <button>좋아요 많은 순</button>
     </div>
   </div>
@@ -15,12 +15,30 @@ export default {
 
 <style lang="scss" scoped>
 .count-and-sort-wrapper {
-  height: 40px;
-  border-bottom: 1px solid $color-heavy-gray;
+  height: 60px;
+  // background-color: $color-very-light-gray;
   .count-and-sort-container {
     padding: 10px 20px;
     display: flex;
     justify-content: space-between;
+    align-items: end;
+    span {
+      color: $color-very-heavy-gray;
+      font-weight: $font-weight-huge;
+      height: auto;
+      line-height: 2;
+    }
+    button {
+      width: 100px;
+      height: $button-medium-height;
+      border-radius: $border-radious/2;
+      border-width: 0.5px;
+      border-color:  $color-light-gray;
+      background-color: $color-button;
+      color: $fonr-color-button;
+      font-weight: $font-weight-huge;
+      font-size: $font-small;
+    }
   }
 }
 </style>

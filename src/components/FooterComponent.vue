3b7db0e2cb379d2b78fe5b18667c756e00683161
yundonghui <dhsimpson@donnie4567.com>
2022-10-13 00:27:52 +0900

    feat | footer 구현

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,11 @@
 <template>
-  <a target="_blank" href="https://icons8.com/icon/24988/리그-오브-레전드">리그 오브 레전드</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+  <pre>
+    만든 사람 : 박규영 | 윤동희
+    문의 및 버그 제보 : qeadzc9503@naver.com (규리나 이메일 쓰까)
+
+    Thanks to
+    <a target="_blank" href="https://icons8.com/icon/24988/리그-오브-레전드">리그 오브 레전드</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+  </pre>
 </template>
 
 <script>
@@ -8,6 +14,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+pre {
+  margin: 20px 0 0;
+  padding: 10px 20px;
+  line-height: 1.3;
+  text-align: left;
+  white-space: pre-line;
+  background-color: white;
+  border-top: 1px solid $color-light-gray;
+  color: $color-very-heavy-gray;
+  a {
+    text-decoration: none;
+    color: $color-very-heavy-gray;
+  }
+}
 </style>

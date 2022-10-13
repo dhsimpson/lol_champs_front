ec079e3f9a23ac8807d71a78e20124bfd37eeb3d
yundonghui <dhsimpson@donnie4567.com>
2022-10-13 15:50:39 +0900

    feat | 팀 선수 필터 모달 추가

---
+++ src/components/modal/SortModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>정렬 모달</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+    width: 200px;
+    height: 400px;
+    background-color: white;
+    // z-index: 1;
+    position: absolute;
+    top: 0;
+}
+</style>
---
+++ src/components/modal/TeamPlayerModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>팀, 선수 선택 모달</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+    width: 500px;
+    height: 200px;
+    background-color: white;
+    // z-index: 1;
+    position: absolute;
+    top: 100%;
+}
+</style>

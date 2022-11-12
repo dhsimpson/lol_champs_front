1838099c26c91f0cd3aed96f6f4d0128358c0d88
yundonghui <dhsimpson@donnie4567.com>
2022-11-12 15:22:44 +0900

    refactor | 페이징 쿼리방식 start,end 로 변경

--- src/store/main/actions.js
+++ src/store/main/actions.js
@@ -7,12 +7,14 @@ const matchUrl = '/v1/lck-match/'; // match == viedo (매치영상)
 
 export default {
     FETCH_VIDEO_LIST: async ({commit}, params) => {
+        Object.assign(params, {start: 0, end: 10});
         const queryParam = Object.entries(params).reduce((acc, [key, value]) => acc += `${key}=${value}&`, '?');
         const videoList = await axios.get(`${apiBaseUrl}${matchUrl}${queryParam}`);
         commit('SET_VIDEO_LIST', videoList.data.data.slice(0,10));//추후에 slice 제거
     },
     ADD_VIDEO_LIST: async ({state, commit}, params) => {
-        params.page = state.pageNum++;
+        const page = state.pageNum++;
+        Object.assign(params, {start: page*10, end: (page+1)*10})
         const queryParam = Object.entries(params).reduce((acc, [key, value]) => acc += `${key}=${value}&`, '?');
         const videoList = await axios.get(`${apiBaseUrl}${matchUrl}${queryParam}`);
         commit('ADD_VIDEO_LIST', videoList.data.data.slice(state.pageNum*10,(state.pageNum+1)*10));//temp: 페이지x10, 페이지+1 x 10.... 추후에 slice 제거
